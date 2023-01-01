dfc7b323810472bb2c50c83ff67f81b3b7e058ba
Rafael Miranda <rafaelpadovezi@gmail.com>
2023-01-01 14:41:07 -0300

    feat: add authentication with github

--- README.md
+++ README.md
@@ -31,3 +31,25 @@ After running the app again the following dbs were created:
  backstage_plugin_auth
  backstage_plugin_search
 ```
+
+### Auth - using Github
+
+#### Import Groups and Users from github organization: https://backstage.io/docs/integrations/github/org
+
+Install Github Org Entity Provider and add the setup code to the `catalog.ts`.
+```sh
+yarn add --cwd packages/backend @backstage/plugin-catalog-backend-module-github
+```
+
+When using a PAT, requires the following permissions: 
+
+- `read:org`
+- `read:user`
+- `user:email`
+
+#### Login with Github
+
+Three steps are required:
+- Configure the OAuth App on [Github](https://github.com/settings/developers) and add the configuration to the config files
+- Add sign-in option to the frontend.
+- In the backend, use the `usernameMatchingUserEntityName` as an identity provider.
--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,11 +2,27 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
+  // The org URL below needs to match a configured integrations.github entry
+  // specified in your app-config.
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'github',
+      orgUrl: 'https://github.com/catcoon-inc',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
